09e74b5316c149711b13c9f3b825bab18b4568b1
Aparna Raj R <74713594+AparnaRaj28@users.noreply.github.com>
2022-04-14 11:24:36 +0530

    Add files via upload

---
+++ Spring_Assignment/SpringAssignment_IoC/src/main/java/com/assignment/ioc/Mammals.java
@@ -0,0 +1,29 @@
+package com.assignment.ioc;
+
+public class Mammals extends Animal
+{
+    private String color;
+
+	public Mammals(String animal_name, String animal_type, String color)
+	{
+		super(animal_name, animal_type);
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+   public String Display()
+   {
+	 return "Name of the mammal is "+animal_name+" its type is "+animal_type+" Color is "+color ; 
+   }
+   
+
+    
+}
---
+++ Spring_Assignment/SpringAssignment_IoC/src/main/java/com/assignment/ioc/config/Animal.java
@@ -0,0 +1,41 @@
+package com.assignment.ioc.config;
+
+public class Animal 
+{
+   public String animal_name;
+   public String animal_type;
+   
+public Animal(String animal_name, String animal_type) 
+{
+	
+	this.animal_name = animal_name;
+	this.animal_type = animal_type;
+}
+
+public Animal() 
+{
+	
+}
+
+public String getAnimal_name() {
+	return animal_name;
+}
+
+public void setAnimal_name(String animal_name) {
+	this.animal_name = animal_name;
+}
+
+public String getAnimal_type() {
+	return animal_type;
+}
+
+public void setAnimal_type(String animal_type) {
+	this.animal_type = animal_type;
+}
+public String Display()
+{
+	return "Name of the animal is "+animal_name+" and type is "+animal_type;
+}
+
+
+}
